c289d1781ff751f704e13f936a3151d64ec0d123
dopeldev <322kuroneko@gmail.com>
2023-03-23 05:30:36 -0500

    config_checker

---
+++ python_playground/config_checker/config_checker.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+"""
+-------------------------------------------------------------------------------
+Author: Dopeldev
+repo: https://www.github.com/dopeldev
+-------------------------------------------------------------------------------
+
+This script checks the configuration files from $HOME/.config of user accounts on a 
+Linux system and compares them with the configuration files from a remote server.
+if the configuration files are different, the script will copy the configuration files
+from the remote server to the local machine.
+
+first step: check if the user has a .config directory in $HOME if not exist then 
+straight copy the remote .config directory to the local machine.
+second step: compare the files in the local machine .config directory with the files 
+in the remote machine .config directory if not equal copy the remote file to the local machine 
+straight copy this file to the local machine.
+"""
+
+import sys
+import filecmp
+import requests
+
