6e5022d863bd6f6fab2c920c9bd5f7e911c31a87
dopeldev <322kuroneko@gmail.com>
2023-03-23 15:54:27 -0500

    add a logger into the spider

--- python_playground/config_checker/config_checker.py
+++ python_playground/config_checker/config_checker.py
@@ -20,11 +20,38 @@ straight copy this file to the local machine.
 import sys
 import filecmp
 import requests
+import logging as log
+
+class ConfigChecker:
+    def __init__(self):
+        self.logger = self.log()
+        pass
+    
+    # log instance
+    def log(self) -> log.Logger:
+        logger = log.getLogger(__name__)
+        
+        return logger
+        
+    # handling the request
+    def handle_request(self, url) -> requests.models.Response:
+        try:
+            response = requests.get(url)
+            return response
+        except Exception as e:
+            
+            print(e)
+            sys.exit(1)
+    
+    def status_code(self, response) -> bool:
+        if response.status_code != 200:
+            print("Error: {}".format(response.status_code))
+            sys.exit(1)
+        else:
+            print("Success: {}".format(response.status_code))
+            return True
+
+    # more stuff to come
 
 if __name__ == "__main__":
-    # check if the user has a .config directory in $HOME if not exist then 
-    # straight copy the remote .config directory to the local machine.
-    # second step: compare the files in the local machine .config directory with the files 
-    # in the remote machine .config directory if not equal copy the remote file to the local machine 
-    # straight copy this file to the local machine.
     pass
