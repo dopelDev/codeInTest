4167555d3fb331e6575ff7c975f83ba8e798c42f
dopeldev <322kuroneko@gmail.com>
2023-03-23 05:46:40 -0500

    testing starshiip

--- python_playground/config_checker/config_checker.py
+++ python_playground/config_checker/config_checker.py
@@ -21,3 +21,10 @@ import sys
 import filecmp
 import requests
 
+if __name__ == "__main__":
+    # check if the user has a .config directory in $HOME if not exist then 
+    # straight copy the remote .config directory to the local machine.
+    # second step: compare the files in the local machine .config directory with the files 
+    # in the remote machine .config directory if not equal copy the remote file to the local machine 
+    # straight copy this file to the local machine.
+    pass
