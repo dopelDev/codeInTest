28dc27d8f2bbb3813d958bdbf0fc5b96058f8c83
dopeldev <322kuroneko@gmail.com>
2023-03-23 20:56:50 -0500

    definite get_remote_config_dir

--- python_playground/config_checker/config_checker.py
+++ python_playground/config_checker/config_checker.py
@@ -26,11 +26,17 @@ import logging as log
 class ConfigChecker:
     def __init__(self):
         self.logger = self.log()
-        pass
+        self.local_config_dir = os.environ["HOME"] + "/.config"
+        self.remote_config_dir = self.get_remote_parameters()
+        self.response_status_sucess =  self.status_code(self.handle_request(""))
     
     # get the environment parameters
     def get_environment_settings(self):
         self.working_directory = os.getcwd()
+
+    def get_remote_parameters(self) -> str:
+        url = sys.argv[1]
+        return url
 
     # logger instance
     def log(self) -> log.Logger:
@@ -51,10 +57,11 @@ class ConfigChecker:
     def handle_request(self, url) -> requests.models.Response:
         try:
             response = requests.get(url)
+            self.logger.info("Request Success: {}".format(response.status_code))
             return response
         except Exception as e:
-            
             print(e)
+            self.logger.error("Request Error: {}".format(e))
             sys.exit(1)
     
     def status_code(self, response) -> bool:
