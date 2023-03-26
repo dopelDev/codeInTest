a89bb293264e070b481dbca3821f139ade3d2fcd
dopeldev <322kuroneko@gmail.com>
2023-03-26 05:29:10 -0500

    rename some functions and create a new function:   function run

--- python_playground/config_checker/config_checker.py
+++ python_playground/config_checker/config_checker.py
@@ -13,7 +13,7 @@ from the remote server to the local machine.
 first step: check if the user has a .config directory in $HOME if not exist then 
 straight copy the remote .config directory to the local machine.
 second step: compare the files in the local machine .config directory with the files 
-in the remote machine .config directory if not equal copy the remote file to the local machine 
+in the remote machine .config directory i. not equal copy the remote file to the local machine 
 straight copy this file to the local machine.
 """
 
@@ -26,12 +26,13 @@ import logging as log
 class ConfigChecker:
     def __init__(self):
         self.logger = self.log()
-        self.local_config_dir =  self.get_local_parameters()
-        self.remote_config_dir = self.get_remote_parameters()
-        self.temporary_dir = self.get_temporary_dir()
-        self.response_status_sucess =  self.status_code(self.handle_request(self.remote_config_dir))
+        self.local_config_dir_path =  self.get_local_parameters()
+        self.remote_url_depository = self.get_url_depository()
+        self.temporary_dir_path = self.get_temporary_dir()
+        self.response_status_sucess =  self.status_code(self.handle_request(self.remote_url_depository))
+        self.temporary_dir_exists = self.create_temporary_dir()
 
-    # get the environment parameters
+    # get the environment parameters at initialization
     def get_local_parameters(self) -> str:
         self.logger.info("Local parameters initialized successfully")
         return os.environ["HOME"] + "/.config"
@@ -40,13 +41,13 @@ class ConfigChecker:
         self.logger.info("Temporary directory initialized successfully")
         return os.environ["HOME"] + "/.config/temp"
 
-    def get_remote_parameters(self) -> str:
+    def get_url_depository(self) -> str:
         if len(sys.argv) <= 1:
-            msg_error = "No URL provided"
+            msg_error = "Error: No URL provided"
             self.logger.error(msg_error)
             raise TypeError(msg_error)
         else:
-            self.logger.info("Remote parameters initialized successfully")
+            self.logger.info("Scuccess: URL provided")
             url = sys.argv[1]
             return url
 
@@ -85,10 +86,12 @@ class ConfigChecker:
             self.logger.info("Success: {}".format(response.status_code))
             print("Success: {}".format(response.status_code))
             return True
-    
+
+    # functions tools for function run
+        # handle data from remote depository
     def dump_response(self, response : requests.models.Response) -> None:
-        if self.response_status_sucess:
-            with open(self.temporary_dir + "/config.tar.gz", "wb") as f:
+        if self.response_status_sucess and self.temporary_dir_exists:
+            with open(self.temporary_dir_path + "/config.tar.gz", "wb") as f:
                 f.write(response.content)
                 self.logger.info("Response dumped successfully")
         else:
@@ -96,23 +99,64 @@ class ConfigChecker:
             sys.exit(1)
 
     def extract_tar(self) -> None:
-        if self.response_status_sucess:
-            os.system("tar -xzf {} -C {}".format(self.temporary_dir + "/config.tar.gz", self.temporary_dir))
+        if self.response_status_sucess and self.temporary_dir_exists:
+            os.system("tar -xzf {} -C {}".format(self.temporary_dir_path + "/config.tar.gz", self.temporary_dir_path))
             self.logger.info("Tar extracted successfully")
         else:
             self.logger.error("Tar not extracted")
             sys.exit(1)
     
-    # compare the files from the local machine and the remote machine
+    # compare the files from the local machine and the remote machine!!!!
+    
+    def create_temporary_dir(self) -> bool:
+        if not os.path.exists(self.temporary_dir_path):
+            try:
+                os.mkdir(self.temporary_dir_path)
+            except PermissionError as e:
+                self.logger.error("Permission denied: {}".format(e))
+                sys.exit(1)
+            self.logger.info("Temporary directory created successfully")
+        else:
+            self.logger.info("Temporary directory already exists")
+        return True
+
+    def check_local_config_files_exists(self) -> bool:
+        # code here
+        return True
+    
+    def get_list_config_files(self) -> list:
+        # code here
+        return []
+
+    def compare_files(self, local_file : str, remote_file : str) -> bool:
+        # code here
+        return True
+
+    def update_config_files(self, local_file : str, remote_file : str) -> None:
+        # code here
+        pass
+
+    def get_remote_list_config_files(self):
+        if not os.path.exists(self.temporary_dir_path):
+            self.logger.error("Temporary directory does not exist")
+            raise FileNotFoundError("Temporary directory does not exist")
+        else:
+            self.logger.info("Temporary directory exists")
+            return os.listdir(self.temporary_dir_path)
 
     # check and clean up
 
     def remove_temporary_dir(self) -> None:
-        if os.path.exists(self.temporary_dir):
-            os.rmdir(self.temporary_dir)
+        if os.path.exists(self.temporary_dir_path):
+            os.rmdir(self.temporary_dir_path)
             self.logger.info("Temporary directory removed")
         else:
             self.logger.info("Temporary directory does not exist")
+    
+    # run function
+    # this function will be called others functions after the initialization
+    def run(self) -> None:
+        pass
 
 if __name__ == "__main__":
     pass
