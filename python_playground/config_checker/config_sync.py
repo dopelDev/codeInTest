3fc277a67fc486efa993bf342ffe200851533b39
dopeldev <322kuroneko@gmail.com>
2023-04-03 03:33:47 -0500

    add files test_config_sync.py log_config.json test_config_sync.py test_config_sync.json

---
+++ python_playground/config_checker/config_sync.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+"""
+This script is used to sync the config files from the remote source to the local files.
+"""
+
+import argparse
+import os
+import requests
+import filecmp
+
